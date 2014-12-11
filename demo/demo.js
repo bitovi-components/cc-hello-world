794a5927c26f12b78268412bc2fe69e2a8edff83
Josh Marchán <sykopomp@sykosomatic.org>
2014-12-10 21:06:37 -0600

    Much nicer toplevel with autoloading

--- demo/demo.js
+++ demo/demo.js
@@ -1,27 +1,8 @@
 // Libraries
-import can from "can";
-import "can/view/stache/stache";
+import autoload from "util/template-autoloader";
 
 // Components
 import "cc-hello-world";
 
-// Template
-import template from "./demo.stache!";
+autoload();
 
-export default can.Component.extend({
-	tag: "cc-hello-world-demo",
-	template: template,
-	scope: {
-		target: "World"
-	}
-});
-
-// NOTE: This is something CanJS will eventually support automatically in the
-//       future. For the time being, we use this hack only for the toplevel of
-//       applications (such as this demo). Regular components shouldn't try to
-//       do this themselves.
-can.$(() => {
-	can.$("cc-hello-world-demo").each((i, el) => {
-		can.$(el).replaceWith(can.stache(el.outerHTML)(new can.Map()));
-	});
-});
