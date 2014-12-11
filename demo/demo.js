86b7d65854ee75379aee16f76719ff787ecef5c9
Kat Marchán <kzm@sykosomatic.org>
2014-12-10 19:43:17 -0600

    Demo works locally and remotely now

--- demo/demo.js
+++ demo/demo.js
@@ -18,9 +18,10 @@ export default can.Component.extend({
 
 // NOTE: This is something CanJS will eventually support automatically in the
 //       future. For the time being, we use this hack only for the toplevel of
-//       applications (such as this demo)
+//       applications (such as this demo). Regular components shouldn't try to
+//       do this themselves.
 can.$(() => {
 	can.$("cc-hello-world-demo").each((i, el) => {
-		can.$(el).replaceWith(can.stache(el.outerHTML)());
+		can.$(el).replaceWith(can.stache(el.outerHTML)(new can.Map()));
 	});
 });
